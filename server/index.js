60eda23e1cb121a43bb0f52ec80a6972ff6cb224
Rahul-R79 <rahul.devworks@gmail.com>
2026-01-07 13:49:04 +0530

    chore: refactor the cors for production

--- server/index.js
+++ server/index.js
@@ -8,9 +8,15 @@ dotenv.config();
 ConnectDb();
 
 const app = express();
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    })
+);
+
 app.use(express.json());
 
 app.use("/api/leads", leadRoutes);
